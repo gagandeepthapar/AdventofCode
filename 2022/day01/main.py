5316764bf98337284fe3c638a6a1a5e491962401
gaganthapar12309 <gsthapar@gmail.com>
2023-05-30 20:45:02 -0700

    Adding 2022 directory. Completed Day 1 of 2022 (python)

---
+++ 2022/day01/main.py
@@ -0,0 +1,64 @@
+"""
+Day 1, 2022 of Advent of Code: Python
+"""
+from typing import List
+
+
+DEMO_FLAG = 0
+
+def read_file(filename:str) -> List[int]:
+    """ 
+    Read in file of list of values separated by newlines
+    """
+    value_list: List[List[int]] = []
+    current_list: List[int] = []
+
+    with open(filename, encoding='utf-8') as file_p:
+        file_line = file_p.readline()
+        while file_line:
+            if file_line == '\n':
+                value_list.append(current_list)
+                current_list = []
+            else:
+                current_list.append(int(file_line))
+            file_line = file_p.readline()
+        value_list.append(current_list)
+
+    return value_list
+
+def part_one(filename: str) -> int:
+    """ 
+    exposed function for finding solution
+    """
+    max_val: int = 0
+    elf_colony = read_file(filename=filename)
+    for elf in elf_colony:
+        elf_sum = sum(elf)
+        max_val = max(max_val, elf_sum)
+
+    return max_val
+
+def part_two(filename: str, top_n:int) -> int:
+    """ 
+    Solution for Part 2
+    """
+    elf_sums: List[int] = []
+    elf_colony = read_file(filename=filename)
+    for elf in elf_colony:
+        elf_sums.append(sum(elf))
+
+    sorted_elves = sorted(elf_sums, reverse=True)
+    return sum(sorted_elves[:top_n])
+
+if __name__ == '__main__':
+    if DEMO_FLAG:
+        FILE_NAME = 'demo.txt'
+    else:
+        FILE_NAME = 'input.txt'
+    print(f'FILE: {FILE_NAME}')
+
+    output_one = part_one(FILE_NAME)
+    print(f'Part One: {output_one}')
+
+    output_two = part_two(FILE_NAME, 3)
+    print(f'Part Two: {output_two}')
---
+++ 2022/day01/test.py
@@ -0,0 +1,90 @@
+""" 
+Test cases for Day 1, 2022 Advent of Code
+"""
+
+from main import read_file, part_one, part_two
+
+class TestReadFile:
+    """ 
+    Test class for read file
+    """
+    def test_sanity(self):
+        """ 
+        Sanity check for pytest
+        """
+        test = 1+2
+        expect = 3
+        assert test == expect
+
+
+    def test_demo(self):
+        """ 
+        Copy of Demo values 
+        """
+        file = "demo.txt"
+        max_elf = read_file(file)
+        assert max_elf == [[1_000, 2_000, 3_000],
+                           [4_000],
+                           [5_000, 6_000],
+                           [7_000,8_000,9_000],
+                           [10_000]]
+
+class TestDemoOne:
+    """ 
+    Test class for pytest
+    """
+    def test_sanity(self):
+        """ 
+        Sanity check for pytest
+        """
+        test = 1+2
+        expect = 3
+        assert test == expect
+
+
+    def test_demo(self):
+        """ 
+        Solution provided by Advent of Code
+        """
+        file = "demo.txt"
+        max_elf = part_one(file)
+        assert max_elf == 24000
+
+
+    def test_input_one(self):
+        """
+        Solution tested against AoC
+        """
+        file = 'input.txt'
+        max_elf = part_one(file)
+        assert max_elf == 66719
+
+class TestDemoTwo:
+    """ 
+    Test class for pytest, part 2
+    """
+    def test_sanity(self):
+        """ 
+        Sanity check for pytest
+        """
+        test = 1+2
+        expect = 3
+        assert test == expect
+
+
+    def test_demo(self):
+        """ 
+        Solution provided by Advent of Code
+        """
+        file = "demo.txt"
+        max_elf = part_two(file, 3)
+        assert max_elf == 45000
+
+
+    # def test_input_one(self):
+    #     """
+    #     Solution tested against AoC
+    #     """
+    #     file = 'input.txt'
+    #     max_elf = part_two(file)
+    #     assert max_elf == 66719
