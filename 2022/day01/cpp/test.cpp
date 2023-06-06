4c29255d2fb2264fb8e4e7cd609b9b57c3df3df6
gaganthapar12309 <gsthapar@gmail.com>
2023-06-06 12:06:24 -0700

    added gtest to assert testing values

--- 2022/day01/cpp/test.cpp
+++ 2022/day01/cpp/test.cpp
@@ -1,39 +1,43 @@
-#include "main.hpp"
+#include "src.hpp"
+#include "utils.hpp"
 
+#include <climits>
 #include <iostream>
 #include <gtest/gtest.h>
 
 using namespace std;
 
-TEST(DAY01, READ_TEST){
-	// expect data
-	vector<string> expect = {"1000", "2000", "4000"};
-
-	// test data
-	vector<string> data = myread("test.txt");
-	
-	// ensure non-empty file
-	if(data.size() == 0){
-		cout << "NOTHING IN FILE" << endl;
-		FAIL();
-	}
-
-	if(data.size() != expect.size()){
-		cout << "Differently sized vectors. Test: " << data.size() << "; Expected: " << expect.size() << endl;
-		FAIL();
-	}
-
-	for(int i = 0; i < data.size(); i ++){
-		EXPECT_EQ(data[i], expect[i]) << " Position: " << i;
-	}
-
-}
-
-TEST(DAY01, Basic){
+// ensure gtest is working
+TEST(DAY01, Sanity){
 	EXPECT_EQ(1+1, 2);
 }
 
-TEST(DAY01, Pass){
-	EXPECT_EQ(1-1, 0);
+// test demo values for part 1
+TEST(DAY01, DemoOne){
+  int test = part_one("../demo.txt");
+  int expect = 24000;
+
+  EXPECT_EQ(test, expect);
 }
 
+// test input values for part 1
+TEST(DAY01, InputOne){
+  int test = part_one("../input.txt");
+  int expect = 66719;
+
+  EXPECT_EQ(test, expect);
+}
+
+// test demo values for part 2
+TEST(DAY01, DemoTwo){
+  int test = part_two("../demo.txt");
+  int expect = 45000;
+  EXPECT_EQ(test, expect);
+}
+
+// test input values for part 2
+TEST(DAY01, InputTwo){
+  int test = part_two("../input.txt");
+  int expect = 198551;
+  EXPECT_EQ(test, expect);
+}
